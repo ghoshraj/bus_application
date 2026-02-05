596ae4e7b36ee09b79d7b00028776a13fc3cb7ea
Rajesh Ghosh <rajgaming688@gmail.com>
2026-02-05 21:56:13 +0530

    add correct response in swagger

--- Security/src/main/java/com/example/demo/model/AuthenticationResponse.java
+++ Security/src/main/java/com/example/demo/model/AuthenticationResponse.java
@@ -1,13 +1,19 @@
 package com.example.demo.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import static com.example.demo.constant.ModelConstants.LOGIN_MESSAGE_EXAMPLE;
+import static com.example.demo.constant.ModelConstants.TOKEN_EXAMPLE;
 
 @Data
 @AllArgsConstructor
 public class AuthenticationResponse {
 
+    @Schema(example = TOKEN_EXAMPLE)
     private String token;
+    @Schema(example = LOGIN_MESSAGE_EXAMPLE)
     private String message;
 
 }
--- Security/src/main/java/com/example/demo/model/RegisterResponse.java
+++ Security/src/main/java/com/example/demo/model/RegisterResponse.java
@@ -1,12 +1,18 @@
 package com.example.demo.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+
+import static com.example.demo.constant.ModelConstants.*;
 
 @Data
 public class RegisterResponse {
 
+    @Schema(example = USER_ID_EXAMPLE)
     private int id;
+    @Schema(example = USERNAME_EXAMPLE)
     private String name;
+    @Schema(example = USER_EMAIL_EXAMPLE)
     private String email;
 
 }
